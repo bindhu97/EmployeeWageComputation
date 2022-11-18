301b507dc7c2b805d9add3c5b700251651a1b4c4
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-18 19:04:50 +0530

    Employee Wage Switch

---
+++ EmployeeWageComputation/src/main/java/com/bridgelabz/EmployeeWageComputation/EmployeeWageSwitch.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.EmployeeWageComputation;
+
+public class EmployeeWageSwitch {
+
+	public static void main(String[] args) {
+System.out.println("Welcome to Employee Wage Computation Program");
+        
+		int wagePerHour = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+        int dailyWage = 0;
+
+        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+//uc3-part time employee wage
+        /*if (employeeCheck == 2) {
+            dailyWage = fullTimeHour * wagePerHour;
+            System.out.println("Employee is present full time");
+        } else if (employeeCheck == 1) {
+            dailyWage = partTimeHour * wagePerHour;
+            System.out.println("Employee is present half time");
+        } else {
+            System.out.println("Employee is absent");
+        }
+        System.out.println("Employee total wage is: " +dailyWage); */
+//UC4-using switch case
+        switch (employeeCheck) {
+        case 2:
+            dailyWage = fullTimeHour * wagePerHour;
+            System.out.println("Employee is present full time");
+            break;
+        case 1:
+            dailyWage = partTimeHour * wagePerHour;
+            System.out.println("Employee is present half time");
+            break;
+        default:
+            System.out.println("Employee is absent");
+    }
+    System.out.println("Employee total wage is: " +dailyWage);
+    }
+}
+
